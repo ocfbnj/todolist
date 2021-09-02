9aecb668f424620a31f2e99aec8741db6eef19c8
ocfbnj <ocfbnj@qq.com>
2021-09-02 11:39:05 +0800

    change side menu style

--- todolist/Controllers/MainViewController.swift
+++ todolist/Controllers/MainViewController.swift
@@ -12,6 +12,8 @@ class MainViewController: UIViewController {
     private var addItemVC = AddItemViewController()
     private var navVC: UINavigationController?
     
+    static let sideMenuWidth = CGFloat(300)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,36 +24,55 @@ class MainViewController: UIViewController {
         let navVC = UINavigationController(rootViewController: homeVC)
         self.navVC = navVC
         
-        addChild(menuVC)
         addChild(navVC)
+        addChild(menuVC)
         addChild(addItemVC)
         
-        view.addSubview(menuVC.view)
         view.addSubview(navVC.view)
+        view.addSubview(menuVC.view)
         view.addSubview(addItemVC.view)
+        
+        menuVC.view.frame.size.width = Self.sideMenuWidth
+        menuVC.view.isHidden = true
         addItemVC.view.isHidden = true
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        menuVC.view.frame.size.width = Self.sideMenuWidth
     }
 }
 
 extension MainViewController {
+    fileprivate func openMenu() {
+        menuVC.view.isHidden = false
+        menuVC.view.frame.origin.x = -menuVC.view.frame.size.width
+        UIView.animate(withDuration: 0.33, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: .curveEaseInOut) {
+            self.menuVC.view.frame.origin.x = 0
+        } completion: { done in
+            if done {
+                self.menuState = .opened
+            }
+        }
+    }
+    
+    fileprivate func closeMenu() {
+        UIView.animate(withDuration: 0.33, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: .curveEaseInOut) {
+            self.menuVC.view.frame.origin.x = -self.menuVC.view.frame.size.width
+        } completion: { done in
+            if done {
+                self.menuVC.view.isHidden = true
+                self.menuState = .closed
+            }
+        }
+    }
+    
     private func toggleSideMenu() {
         switch menuState {
         case .closed:
-            UIView.animate(withDuration: 0.33, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: .curveEaseInOut) {
-                self.navVC?.view.frame.origin.x = self.homeVC.view.frame.size.width - 100
-            } completion: { done in
-                if done {
-                    self.menuState = .opened
-                }
-            }
+            openMenu()
         case .opened:
-            UIView.animate(withDuration: 0.33, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: .curveEaseInOut) {
-                self.navVC?.view.frame.origin.x = 0
-            } completion: { done in
-                if done {
-                    self.menuState = .closed
-                }
-            }
+            closeMenu()
         }
     }
 }
